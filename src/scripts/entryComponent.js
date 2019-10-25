6c27883861afffd3a5ef847f3a784a4243b9683b
John Long <jlcomp45@gmail.com>
2019-10-25 07:56:52 -0500

    refactored functions to make a method in an object per challent in je5 - tested in browser - exercise completed

--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -1,9 +1,11 @@
-const journalEntryComponent = (journalEntry) => {
-    return `
+const toDom = {
+    journalEntryComponent: (journalEntry) => {
+        return `
     <div class=entries>
         <div class="date">${journalEntry.date}</div>
         <div class="concepts">${journalEntry.conceptCovered}</div>
         <div class="content">${journalEntry.contentsEntered}</div>
         <div class="mood">${journalEntry.moodOfDay}</div>
         `
+    }
 }
