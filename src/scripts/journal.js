ab2c2518d41e52154c899500c125de22ce1a39f8
John Long <jlcomp45@gmail.com>
2019-10-30 07:42:27 -0500

    working on populating info to api

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -8,3 +8,25 @@ API.myData().then(parsedEntries => {
 // calls the initial fetch of data imported from data.js
 
 // waits for that data to be converted to json and 2nd .then executes 
+
+// start of Journal exercise 7 - doing all in main js file for testing before modularizing
+let date = document.getElementById(".date")
+let conceptsCovered = document.getElementById(".concepts")
+let content = document.getElementById(".content")
+let mood = document.getElementById(".mood")
+
+
+const createJournalEntry = (date, conceptsCovered, content, mood) => {
+    return {
+        date: date,
+        conceptsCovered: conceptsCovered,
+        content: content,
+        mood: mood
+    }
+}
+document.getElementById("submit-button").addEventListener("click", createJournalEntry(date.value, conceptsCovered.value, content.value, mood.value))
+
+
+
+
+
