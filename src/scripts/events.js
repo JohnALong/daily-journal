976f67c73460bb1a9146a4a85817c0365fc9aaee
John Long <jlcomp45@gmail.com>
2019-11-01 09:05:50 -0500

    working on journal 8, objectified function in events.js, and successfully targeted mood buttons with click event

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -6,17 +6,28 @@ const events = {
         let conceptsCovered = document.getElementById("conceptsCovered").value
         let content = document.getElementById("content").value
         let mood = document.getElementById("mood").value
-        const entryObject = createJournalEntry(date, conceptsCovered, content, mood)
+        const entryObject = events.createJournalEntry(date, conceptsCovered, content, mood)
         console.log("please let this work", entryObject)
         API.newEntry(entryObject).then(API.myData).then(allEntries.entryToDom)
+    },
+    createJournalEntry: (date, conceptsCovered, content, mood) => {
+        return {
+            date: date,
+            conceptsCovered: conceptsCovered,
+            content: content,
+            mood: mood
+        }
     }
 }
-const createJournalEntry = (date, conceptsCovered, content, mood) => {
-    return {
-        date: date,
-        conceptsCovered: conceptsCovered,
-        content: content,
-        mood: mood
-    }
-}
+
+
+// let moodArray = document.getElementsByName("moodButton")
+// console.log("mood", moodArray)
+
+// moodArray.forEach(mood => {
+//     mood.addEventListener("click", event => {
+//         const thisMood = event.target.value
+//         console.log("values", thisMood)
+//     })
+// })
 export default events;
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,12 +2,27 @@ import API from "./data.js"
 import allEntries from "./entriesDom.js"
 import events from "./events.js"
 // import toDom from "./entryComponent.js"
-
 allEntries.formToDom()
 
 API.myData().then(parsedEntries => {
     allEntries.entryToDom(parsedEntries)
 })
+
+document.getElementById("submit-button").addEventListener("click", events.handleSendIt)
+
+// new code
+let moodArray = document.getElementsByName("moodButton")
+console.log("mood", moodArray)
+
+moodArray.forEach(mood => {
+    mood.addEventListener("click", event => {
+        const thisMood = event.target.value
+        console.log("values", thisMood)
+    })
+})
+// Once you have successfully retrieved the value of ok, happy, or sad based on which radio button was clicked, you need to filter all of the journal entries. The most straightforward way of doing this is to invoke the getJournalEntries() method on your data manager object - which will get all entries - and then use the filter() array method to extract only the entries that have the same mood as the one the user clicked on.
+
+// Once you have filtered the entries by mood, invoke the function that renders the HTML representations to the DOM and pass it the filtered array of entries.
 
 // calls the initial fetch of data imported from data.js
 
@@ -18,6 +33,5 @@ API.myData().then(parsedEntries => {
 // factory function to create object of entry
 
 
-document.getElementById("submit-button").addEventListener("click", events.handleSendIt)
 
 
